ef123e4ce5a72155ebb08349292d9c0821a69df2
Abdurehman339 <abdurrehman.musharaf@gamil.com>
2023-12-14 00:37:33 +0500

    Basic Register Screen

---
+++ lib/view/register_view.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const TextField(
+          autocorrect: false,
+          enableSuggestions: false,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            hintText: "Enter email here",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+          ),
+        ),
+        const TextField(
+          autocorrect: false,
+          enableSuggestions: false,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+              hintText: "Enter password here",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              )),
+        ),
+        TextButton(
+          onPressed: () {
+            print("Register button pressed");
+          },
+          child: Text("Register"),
+        ),
+      ],
+    );
+  }
+}
